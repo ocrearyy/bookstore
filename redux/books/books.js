d60adea6234391957001bb40aaa6f92c24fcc59d
oshanedesign <oshane.design@gmail.com>
2022-05-03 19:25:58 -0500

    add action reducers

---
+++ redux/books/books.js
@@ -0,0 +1,33 @@
+const ADD_BOOK = 'BOOK_ADDED';
+const REMOVE_BOOK = 'BOOK_REMOVED';
+
+export const moreBooks = () => ({
+  type: ADD_BOOK,
+});
+
+export const lessBooks = () => ({
+  type: REMOVE_BOOK,
+});
+
+export default bookReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_BOOK: return [
+      ...state,
+      {
+        id: 1,
+        title: 'Title',
+        author: 'Oshane',
+      },
+    ];
+
+    case REMOVE_BOOK: return [
+      ...state,
+      {
+        id: '',
+        title: '',
+        author: '',
+      },
+    ];
+    default: state;
+  }
+};

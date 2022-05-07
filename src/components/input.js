fa0e7b46bfff8c86a7d3dbf5d1516557aa2a5902
oshanedesign <oshane.design@gmail.com>
2022-05-07 16:27:24 -0500

    style input section

--- src/components/input.js
+++ src/components/input.js
@@ -14,10 +14,11 @@ const Input = () => {
   };
   return (
     <div className="input-book">
+      <h1 className="input-h1">ADD NEW BOOK</h1>
       <form onSubmit={handleSubmission}>
-        <input id="title" type="text" placeholder="title" />
-        <input id="author" type="text" placeholder="author" />
-        <button type="submit">Add Book</button>
+        <input className="input-field" id="title" type="text" size="60" placeholder="Book Title" />
+        <input className="input-field" id="author" type="text" size="40" placeholder="Author" />
+        <button className="submit-btn" type="submit">Add Book</button>
       </form>
     </div>
   );
